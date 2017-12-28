feb68541a71d291ebfdfd85e066a7eab5eb48600
Gert <gert@gertrietveld.com>
2017-12-28 17:52:25 +0100

    moved variable declarations closer to use

--- cpp/find/find.cpp
+++ cpp/find/find.cpp
@@ -1,6 +1,5 @@
 #include "opencv2/imgproc/imgproc.hpp"
 #include "opencv2/highgui/highgui.hpp"
-#include <math.h>
 #include <iostream>
 
 using namespace cv;
@@ -13,36 +12,34 @@ int main( int argc, char** argv)
     String imageName = (argc > 1 ) ? argv[1] : "../../data/circle_square.jpg";
 
     Mat image, gray, tresh;
-
     image = imread(imageName, CV_LOAD_IMAGE_COLOR); 
-
     if(! image.data ) // Check for invalid input
     {
         cout <<  "Could not open or find the image" << endl;
         return -1;
     }
-
     cvtColor(image,gray,COLOR_BGR2GRAY);
     threshold(gray,tresh,127,255,0);
 
-
     vector<vector<Point> > contours, squares, circles;
     vector<Vec4i> hierarchy;
-
     findContours(tresh, contours, hierarchy, RETR_TREE, CHAIN_APPROX_SIMPLE);
 
         /// Draw contours
     //Mat drawing = Mat::zeros( tresh.size(), CV_8UC3 );
     int c_size = (int) contours.size();
-    vector<Point> approx;
-    double area;
-    for( int i = 0; i< c_size; i++ ) {
+    for(int i = 0; i< c_size; i++) {
+        vector<Point> approx;
         approxPolyDP(contours[i],approx,0.01*arcLength(contours[i],true),true);
-        area = contourArea(contours[i]);
-        if (10000 < area and area < 15000) {
-            if(approx.size() == 4) {
+        Rect brect = boundingRect(approx);
+        float aspectRatio = brect.width / (float) brect.height;
+        cout << "aspectRatio: " << aspectRatio << endl;
+        double area = contourArea(contours[i]);
+        // ballpark check aspectratio and area
+        if (0.8 < aspectRatio and aspectRatio < 1.2 and 10000 < area and area < 15000) {
+            if(approx.size() >= 4 and approx.size() <= 8) {
                 squares.push_back(contours[i]);
-                cout << "square: " << area << " - " << approx.size() << endl;
+                cout << "square: " << area << " - " << approx.size() << endl;      
             }  
             if(approx.size() > 8) {
                 circles.push_back(contours[i]);
@@ -57,11 +54,13 @@ int main( int argc, char** argv)
     int sq_size = (int) squares.size();
     for( int i = 0; i< sq_size; i++ ) {
         drawContours( image, squares, i, sq_color, 2, LINE_AA, hierarchy, 0 );
+        break;
     }
 
     int ci_size = (int) circles.size();
     for( int i = 0; i< ci_size; i++ ) {
         drawContours( image, circles, i, ci_color, 2, LINE_AA, hierarchy, 0 );
+        break;
     }
 
 
