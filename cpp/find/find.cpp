1a782c5252b74189ad35bc3bed4a1b8ec84f9c64
Gert <gert@gertrietveld.com>
2017-12-28 22:28:55 +0100

    draw cross in center of found items

--- cpp/find/find.cpp
+++ cpp/find/find.cpp
@@ -5,6 +5,9 @@
 using namespace cv;
 using namespace std;
 
+Point getCenter(vector<Point> approx);
+void drawCross(Mat image, vector<Point> approx, Scalar color);
+
 int main( int argc, char** argv)
 {
     namedWindow( "Find square and circle", WINDOW_AUTOSIZE);
@@ -21,51 +24,55 @@ int main( int argc, char** argv)
     cvtColor(image,gray,COLOR_BGR2GRAY);
     threshold(gray,tresh,127,255,0);
 
-    vector<vector<Point> > contours, squares, circles;
+    vector<vector<Point> > contours;
     vector<Vec4i> hierarchy;
     findContours(tresh, contours, hierarchy, RETR_TREE, CHAIN_APPROX_SIMPLE);
 
-        /// Draw contours
-    //Mat drawing = Mat::zeros( tresh.size(), CV_8UC3 );
+    // Mat drawing = Mat::zeros( tresh.size(), CV_8UC3 );
+    Scalar sq_color = Scalar( 0, 255, 0 );
+    Scalar ci_color = Scalar( 255, 0, 0 );
+    bool found_sq = false;
+    bool found_ci = false;
     int c_size = (int) contours.size();
     for(int i = 0; i< c_size; i++) {
         vector<Point> approx;
         approxPolyDP(contours[i],approx,0.01*arcLength(contours[i],true),true);
         Rect brect = boundingRect(approx);
         float aspectRatio = brect.width / (float) brect.height;
-        cout << "aspectRatio: " << aspectRatio << endl;
+        // cout << "aspectRatio: " << aspectRatio << endl;
         double area = contourArea(contours[i]);
-        // ballpark check aspectratio and area
+        // ballpark check aspectratio and area 
         if (0.8 < aspectRatio and aspectRatio < 1.2 and 10000 < area and area < 15000) {
-            if(approx.size() >= 4 and approx.size() <= 8) {
-                squares.push_back(contours[i]);
-                cout << "square: " << area << " - " << approx.size() << endl;      
+            if(found_sq == false and approx.size() >= 4 and approx.size() <= 8) {
+                drawContours( image, contours, i, sq_color, 2, LINE_AA, hierarchy, 0 );
+                // cout << "square: " << area << " - " << approx.size() << endl;  
+                drawCross(image, approx, sq_color);
+                found_sq = true;    
             }  
-            if(approx.size() > 8) {
-                circles.push_back(contours[i]);
-                cout << "circle: " << area << " - " << approx.size() << endl;
+            if(found_ci == false and approx.size() > 8) {
+                drawContours( image, contours, i, ci_color, 2, LINE_AA, hierarchy, 0 );
+                // cout << "circle: " << area << " - " << approx.size() << endl;
+                drawCross(image, approx, ci_color);
+                found_ci = true;
             }      
         }
     }
 
-    Scalar sq_color = Scalar( 0, 255, 0 );
-    Scalar ci_color = Scalar( 255, 0, 0 );
-
-    int sq_size = (int) squares.size();
-    for( int i = 0; i< sq_size; i++ ) {
-        drawContours( image, squares, i, sq_color, 2, LINE_AA, hierarchy, 0 );
-        break;
-    }
-
-    int ci_size = (int) circles.size();
-    for( int i = 0; i< ci_size; i++ ) {
-        drawContours( image, circles, i, ci_color, 2, LINE_AA, hierarchy, 0 );
-        break;
-    }
 
 
     imshow( "Find square and circle", image ); // Show our image inside it.
 
     waitKey(0); // Wait for a keystroke in the window
     return 0;
+}
+
+void drawCross(Mat image, vector<Point> approx, Scalar color) {
+    Point center = getCenter(approx);
+    line(image, Point(center.x-15,center.y),Point(center.x+15,center.y), color, 3);
+    line(image, Point(center.x,center.y-15),Point(center.x,center.y+15), color, 3);
+}
+
+Point getCenter(vector<Point> approx) {
+    Moments m = moments(approx);
+    return  Point((int)m.m10/m.m00,(int)m.m01/m.m00);;
 }
