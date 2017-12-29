01274ef15035a1775afc5b1be6f173f7630cb374
Gert <gert@gertrietveld.com>
2017-12-29 14:36:25 +0100

    updated build script

--- cpp/find/build.sh
+++ cpp/find/build.sh
@@ -4,7 +4,7 @@ echo "compiling..."
 
 if [ "$(uname)" == "Darwin" ]; then
 echo "mac"
-g++ `pkg-config --cflags --libs opencv` find.cpp  -v -Wall -o../build/Find
+g++ `pkg-config --cflags --libs opencv` find.cpp -g -v -Wall -o../build/Find
 
 # expands to:
 # g++ -I/usr/local/Cellar/opencv/3.3.1_1/include/opencv -I/usr/local/Cellar/opencv/3.3.1_1/include 
@@ -18,7 +18,12 @@ g++ `pkg-config --cflags --libs opencv` find.cpp  -v -Wall -o../build/Find
 # find.cpp  -v -WCL4 -o../build/Find
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
 echo "linux"
-g++ -I/usr/local/include/opencv -I/usr/local/include/opencv2 -L/usr/local/lib/ -g -o../build/Find -Wall find.cpp `pkg-config --libs opencv`
-# -lopencv_core -lopencv_imgproc -lopencv_highgui -lopencv_ml -lopencv_video -lopencv_features2d -lopencv_calib3d -lopencv_objdetect -lopencv_contrib -lopencv_legacy -lopencv_stitching
+g++ -I/usr/local/include/opencv -I/usr/local/include/opencv2 -L/usr/local/lib/ -g -o../build/Find -Wall -v find.cpp `pkg-config --libs opencv`
+# /usr/lib/x86_64-linux-gnu/libopencv_calib3d.so -lopencv_calib3d /usr/lib/x86_64-linux-gnu/libopencv_contrib.so -lopencv_contrib /usr/lib/x86_64-linux-gnu/libopencv_core.so -lopencv_core 
+# /usr/lib/x86_64-linux-gnu/libopencv_features2d.so -lopencv_features2d /usr/lib/x86_64-linux-gnu/libopencv_flann.so -lopencv_flann /usr/lib/x86_64-linux-gnu/libopencv_gpu.so -lopencv_gpu 
+# /usr/lib/x86_64-linux-gnu/libopencv_highgui.so -lopencv_highgui /usr/lib/x86_64-linux-gnu/libopencv_imgproc.so -lopencv_imgproc /usr/lib/x86_64-linux-gnu/libopencv_legacy.so -lopencv_legacy 
+# /usr/lib/x86_64-linux-gnu/libopencv_ml.so -lopencv_ml /usr/lib/x86_64-linux-gnu/libopencv_objdetect.so -lopencv_objdetect /usr/lib/x86_64-linux-gnu/libopencv_ocl.so -lopencv_ocl 
+# /usr/lib/x86_64-linux-gnu/libopencv_photo.so -lopencv_photo /usr/lib/x86_64-linux-gnu/libopencv_stitching.so -lopencv_stitching /usr/lib/x86_64-linux-gnu/libopencv_superres.so -lopencv_superres 
+# /usr/lib/x86_64-linux-gnu/libopencv_ts.so -lopencv_ts /usr/lib/x86_64-linux-gnu/libopencv_video.so -lopencv_video /usr/lib/x86_64-linux-gnu/libopencv_videostab.so -lopencv_videostab
 fi
 echo "done"
